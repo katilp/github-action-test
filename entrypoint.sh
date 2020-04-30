dce2f262b806f0fad6c9a2c6662d65a0cef52fec
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-04-30 14:15:04 +0200

    Setting up the release area

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,19 @@ whoami
 cd /home/cmsusr
 pwd
 ls -l
+
+set  -e
+
+#echo "Setting up ${CMSSW_VERSION}"
+echo "Setting up CMSSW_4_2_8"
+source /opt/cms/cmsset_default.sh
+#scramv1 project CMSSW ${CMSSW_VERSION}
+#cd ${CMSSW_VERSION}/src
+scramv1 project CMSSW CMSSW_4_2_8
+cd CMSSW_4_2_8/src
+eval `scramv1 runtime -sh`
+echo "CMSSW should now be available."
+
+#exec "$@"
+pwd
+ls -l
