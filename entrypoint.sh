e258b29e2e178d7d4895f3dcac3f96e88c141165
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-05-02 19:52:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,8 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-pwd
-ls -l
-whoami
+
 cd /home/cmsusr
-pwd
-ls -l
 
 set  -e
 
@@ -20,9 +16,8 @@ source /opt/cms/cmsset_default.sh
 scramv1 project CMSSW CMSSW_4_2_8
 cd CMSSW_4_2_8/src
 eval `scramv1 runtime -sh`
-echo "CMSSW should now be available."
+echo "CMSSW_4_2_8 is at your service."
 
 #exec "$@"
-pwd
-ls -l
-edmDumpEventContent root://eospublic.cern.ch//eos/opendata/cms/hidata/HIRun2011/HIHighPt/RECO/15Apr2013-v1/110000/10EF1D2D-77B4-E211-A652-003048F1C494.root
+edmDumpEventContent root://eospublic.cern.ch//eos/opendata/cms/hidata/HIRun2011/HIHighPt/RECO/15Apr2013-v1/110000/10EF1D2D-77B4-E211-A652-003048F1C494.root > evtdump.txt
+head evtdump.txt
