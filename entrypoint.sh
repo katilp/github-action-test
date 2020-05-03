1d6330638a776c7a0ba13961d7111329cc8a8f8f
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-05-03 18:23:35 +0200

    Leave only exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,17 +19,17 @@ cd $2/src
 eval `scramv1 runtime -sh`
 echo "$2 is at your service."
 
-#exec "$@"
-edmDumpEventContent $1 > evtdump.txt
-head evtdump.txt
-edmProvDump --sort $1 > provdump.txt
-head -100 provdump.txt
+exec "$@"
+#edmDumpEventContent $1 > evtdump.txt
+#head evtdump.txt
+#edmProvDump --sort $1 > provdump.txt
+#head -100 provdump.txt
 
-su root
-mv provdump.txt /github/workspace
-ls -l 
-ls -l /github/workspace
-ls -l /github/home
-ls -l /github/workflow
-echo "::set-output name=another_output::evtdump.txt"
+#su root
+#mv provdump.txt /github/workspace
+#ls -l 
+#ls -l /github/workspace
+#ls -l /github/home
+#ls -l /github/workflow
+#echo "::set-output name=another_output::evtdump.txt"
 
