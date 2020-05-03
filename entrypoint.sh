0943ca4cccb44b5b8c2fe7d69f2743021e922a15
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-05-03 22:05:38 +0200

    check whoami

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ echo "$2 is at your service."
 
 # giving up with this, it gives 'multiple occurences' like this or with just $1 and the input is the full command (???): $(echo $1) 
 edmProvDump --sort $3 > dump.txt
+whoami
 head -10 dump.txt 
 echo $1
 
